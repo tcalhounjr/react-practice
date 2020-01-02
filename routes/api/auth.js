d5683daadddd35fbf540699be42c5edffff3e5aa
tcalhounjr <thomas@nafasi.co>
2020-01-02 04:06:58 -0500

    User login route works with authentication

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const bcrypt = require('bcryptjs');
 
 const neode = require('neode')
     .fromEnv()
     .with({ User: require("../../models/User") });
 
 //@route  GET api/auth
-//@desc   Test route
+//@desc   Get authneticated user token
 //@access Public
 router.get('/', auth, async (req, res) => {
     try {
@@ -26,5 +30,69 @@ router.get('/', auth, async (req, res) => {
         res.status(500).send('Server Error');
     }
 });
+//@route  POST api/auth
+//@desc   Authenticate user and get token for login
+//@access Public
+
+router.post('/', [
+    check('email', 'Please enter a valid email').isEmail(),
+    check('password', 'Invalid login credentials').isLength({ min: 6 })
+],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+
+
+        const { email, password } = req.body;
+
+
+        try {
+            //See if user exists
+
+            let node = await neode.find('User', email);
+
+            if (!node) {
+                return res.status(400).json({ errors: [{ msg: 'Invalid login credentials' }] });
+            }
+
+            let user = {
+                email: node.get('email'),
+                password: node.get('password')
+            };
+
+            const isMatch = await bcrypt.compare(password, user.password);
+
+            if (!isMatch) {
+                return res
+                    .status(400)
+                    .json({ errors: [{ msg: 'Invalid login credentials' }] });
+            }
+
+            //Return jsonwebtoken
+            const payload = {
+                login: {
+                    id: node.id()
+                }
+            }
+
+            jwt.sign(
+                payload,
+                config.get('jwtSecret'),
+                { expiresIn: 360000000 },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token });
+                }
+            );
+
+        } catch (err) {
+            console.log('here I am');
+            console.trace();
+            console.log(err.stack);
+        }
+    });
+
 
 module.exports = router;
